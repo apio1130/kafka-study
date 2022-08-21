c100a34f90189a8c6876a49592c14a70586f2169
kjkim <apio1130@naver.com>
2022-08-22 01:15:02 +0900

    feat(clip3): kafkaTemplate 재정의
    - AutoConfig에서 제공하는 방식과 유사하게 kafkaTemplate을 재정의

--- chap1-clip1/src/main/java/com/fastcampus/chap1clip1/Chap1Clip1Application.java
+++ chap1-clip1/src/main/java/com/fastcampus/chap1clip1/Chap1Clip1Application.java
@@ -1,15 +1,10 @@
 package com.fastcampus.chap1clip1;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.TopicDescription;
-import org.apache.kafka.clients.admin.TopicListing;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Collections;
-import java.util.Map;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class Chap1Clip1Application {
@@ -19,22 +14,9 @@ public class Chap1Clip1Application {
     }
 
     @Bean
-    public ApplicationRunner runner(AdminClient adminClient) {
+    public ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-            // topic 정보 확인
-            Map<String, TopicListing> topics = adminClient.listTopics().namesToListings().get();
-            for (String topicName : topics.keySet()) {
-                TopicListing topicListing = topics.get(topicName);
-                System.out.println(topicListing);
-
-                Map<String, TopicDescription> description = adminClient.describeTopics(Collections.singleton(topicName)).all().get();
-                System.out.println(description);
-
-                if (!topicListing.isInternal()) {
-                    // topic 삭제
-                    adminClient.deleteTopics(Collections.singleton(topicName));
-                }
-            }
+            kafkaTemplate.send("clip3", "Hello, Clip3");
         };
     }
 
