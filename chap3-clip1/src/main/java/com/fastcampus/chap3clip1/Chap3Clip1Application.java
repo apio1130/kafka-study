307d773c37742525d253b7d8d9ca670e97e8b820
kjkim <apio1130@naver.com>
2022-09-10 14:50:30 +0900

    feat(ch03): AdminClient 토픽 정보 조회

--- chap3-clip1/src/main/java/com/fastcampus/chap3clip1/Chap3Clip1Application.java
+++ chap3-clip1/src/main/java/com/fastcampus/chap3clip1/Chap3Clip1Application.java
@@ -1,13 +1,23 @@
 package com.fastcampus.chap3clip1;
 
+import com.fastcampus.chap3clip1.service.KafkaManager;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Chap3Clip1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Chap3Clip1Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaManager kafkaManager) {
+        return args -> {
+            kafkaManager.describeTopicConfigs();
+        };
     }
 
 }
