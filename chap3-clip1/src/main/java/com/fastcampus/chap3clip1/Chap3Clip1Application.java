93df65d4b4cf609fa479ca664452826de90b9899
kjkim <apio1130@naver.com>
2022-09-10 14:28:01 +0900

    feat(ch03): 실습용 프로젝트 생성

---
+++ chap3-clip1/src/main/java/com/fastcampus/chap3clip1/Chap3Clip1Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.chap3clip1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap3Clip1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap3Clip1Application.class, args);
+    }
+
+}
---
+++ chap3-clip1/src/test/java/com/fastcampus/chap3clip1/Chap3Clip1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.chap3clip1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap3Clip1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
