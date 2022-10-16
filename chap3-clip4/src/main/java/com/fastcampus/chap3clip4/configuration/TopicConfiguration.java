57032b85a8c8a4b21f07ab6e7a11a08dcab3b18d
kjkim <apio1130@naver.com>
2022-10-16 22:20:13 +0900

    feat(ch03): 새 토픽 생성

---
+++ chap3-clip4/src/main/java/com/fastcampus/chap3clip4/configuration/TopicConfiguration.java
@@ -0,0 +1,16 @@
+package com.fastcampus.chap3clip4.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class TopicConfiguration {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("clip4").build();
+    }
+
+}
